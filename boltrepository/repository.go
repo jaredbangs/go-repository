7f31d491b1ec213b00688486ef425a8c5cec0113
Jared Bangs <jaredbangs@gmail.com>
2022-07-19 12:26:03 -0700

    Create bucket if doesn't exist when checking HasItem

--- boltrepository/repository.go
+++ boltrepository/repository.go
@@ -94,7 +94,8 @@ func (r *Repository) HasItem(bucketName string, keyName string) (hasItem bool, e
 
 	// retrieve the data
 	err = db.View(func(tx *bolt.Tx) error {
-		bucket := tx.Bucket([]byte(bucketName))
+		// bucket := tx.Bucket([]byte(bucketName))
+		bucket, _ := tx.CreateBucketIfNotExists([]byte(bucketName))
 		if bucket == nil {
 			return fmt.Errorf("Bucket %q not found!", []byte(bucketName))
 		}
